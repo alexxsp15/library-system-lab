e535ab8426256dca3dd824f9680ec89d7f3f2833
Soldatov Olexii <alexsoldatov222@gmail.com>
2026-03-13 11:29:39 +0200

    Initial commit: Hello World

---
+++ main.py
@@ -0,0 +1,9 @@
+while True:
+    name = input("Enter your name: ")
+    if name.strip() == "":
+        print("Name cannot be empty. Try again.")
+    elif not name.isalpha():
+        print("Name must contain only letters. Try again.")
+    else:
+        print(f"Hello, {name}!")
+        break
